8bc62d329a35b7b4e789cd80aa01733bf15f3c08
NSVN-KieuDinhHuan <95573967+NSVN-KieuDinhHuan@users.noreply.github.com>
2022-12-22 16:40:47 +0700

    BE

--- src/main/java/com/newhouse/controller/CategoryController.java
+++ src/main/java/com/newhouse/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.newhouse.controller;
 
+import com.newhouse.model.entity.dish.Dish;
 import com.newhouse.model.entity.dish.category.Category;
 import com.newhouse.model.entity.dish.category.CategoryDto;
 import com.newhouse.model.entity.dish.category.CategoryForm;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -39,10 +41,50 @@ public class CategoryController {
         return new ResponseEntity<>(top5Categories, HttpStatus.OK);
     }
 
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<?> deleteCategory(@PathVariable Long id) {
+        Optional<Category> dishOptional = categoryService.findById(id);
+        if (!dishOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        categoryService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<Category> saveCategory(@ModelAttribute CategoryForm categoryForm) {
         List<MultipartFile> img = categoryForm.getImage();
         Category category = new Category();
+        if (img != null && img.size() != 0) {
+            long currentTime = System.currentTimeMillis();
+            List<String> fileName = img.stream().map(x -> currentTime + x.getOriginalFilename()).collect(Collectors.toList());
+            img.stream().forEach(y ->
+            {
+                try {
+                    FileCopyUtils.copy(y.getBytes(), new File(uploadPath + currentTime + y.getOriginalFilename()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            category.setId(categoryForm.getId());
+            category.setImage(fileName.get(0));
+            category.setName(categoryForm.getName());
+            category.setDescription(categoryForm.getDescription());
+            category.setNumberOfDishes(categoryForm.getNumberOfDishes());
+            categoryService.save(category);
+        }
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+
+    @PostMapping("edit/{id}")
+    public ResponseEntity<Category> updateCategory(@ModelAttribute CategoryForm categoryForm,@PathVariable Long id) {
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        List<MultipartFile> img = categoryForm.getImage();
+        if (!categoryOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Category category = categoryOptional.get();
         if (img != null && img.size() != 0) {
             long currentTime = System.currentTimeMillis();
             List<String> fileName = img.stream().map(x -> currentTime + x.getOriginalFilename()).collect(Collectors.toList());
